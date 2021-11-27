2933d08be9a803f3bca764933fcea8011a095c69
dbernienel <71564993+dbernienel@users.noreply.github.com>
2021-11-27 09:03:30 +0200

    Home page work in progress and fonts added

---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+
+export default function Profile() {
+    return (
+        <div className="profile-container">
+          <div className="profile-parent">
+            <div className="profile-details">
+              <div className="colz">
+                <div className="colz-icon">
+                  <a href="https://web.facebook.com/">
+                    <i className="fa fa-facebook-square" />
+                  </a>
+                  <a href="https://www.instagram.com/instructor_ehizeex/">
+                    <i className="fa fa-google-plus-square" />
+                  </a>
+                  <a href="https://www.instagram.com/instructor_ehizeex/">
+                    <i className="fa fa-instagram" />
+                  </a>
+                  <a href="https://www.instagram.com/instructor_ehizeex/">
+                    <i className="fa fa-youtube-square" />
+                  </a>
+                  <a href="https://twitter.com/Ehiedu_baba">
+                    <i className="fa fa-twitter" />
+                  </a>
+
+                </div>
+                </div>
+
+              <div className= "profile-details-name">
+                  <span className="primary-text">
+                      {" "}
+                      Hello, I'm <span className="highlighted-text"> Bernie</span>
+                  </span>
+              </div>
+              <div className="profile-details-role">
+                  <span className='profile-details-role'>
+                      {" "}
+                      <h1>
+                        {" "}
+                        Digital Forensic Investigator 
+                      </h1>
+                      <span className='profile-role-tagline'>
+                        Combating digital fraud, one case at a time
+                       </span>
+                  </span>
+              </div>
+
+
+            <div className="profile-options">
+            <button className="btn primary-btn"> 
+            {" "}
+            
+            Hire Me {""} 
+            </button>
+            <a href="Daniel Bernie Nel CV.pdf" download="Daniel Bernie Nel CV.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+    
+  );
+        
+    
+}
