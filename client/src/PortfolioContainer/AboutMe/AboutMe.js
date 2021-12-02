6c064a849448c7eaa92e31cd65b4bbee2bec3b68
dbernienel <71564993+dbernienel@users.noreply.github.com>
2021-12-02 23:33:24 +0200

    About Me section and Resune section updated

--- client/src/PortfolioContainer/AboutMe/AboutMe.js
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -12,36 +12,47 @@ export default function AboutMe(props) {
   const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
-  const SCREEN_CONSTSANTS = {
-    description:
-      "Full stack web and mobile developer with background knowledge of MERN stacks with redux, along with a knack of building applications with utmost efficiency. Strong professional with a BSC willing to be an asset for an organization.",
-    highlights: {
-      bullets: [
-        "Full Stack web and mobile development",
-        "Interactive Front End as per the design",
-        "React and React Native",
-        "Redux for State Mnanagement",
-        "Building REST API",
-        "Managing database",
-      ],
-      heading: "Here are a Few Highlights:",
-    },
-  };
-  const renderHighlight = () => {
-    return SCREEN_CONSTSANTS.highlights.bullets.map((value, i) => (
-      <div className="highlight" key={i}>
-        <div className="highlight-blob"></div>
-        <span>{value}</span>
-      </div>
-    ));
-  };
-
-  return (
-    <div
-      className="about-me-container screen-container fade-in"
-      id={props.id || ""}
-    >
-      <div className="about-me-parent">
+    const SCREEN_CONSTANTS = {
+        desciption : "Bernie is blah blah description",
+        highlights:{
+            bullets:[
+               "Bullet 1", 
+               "Bullet 2",
+               "Bullet 3",
+               "Such is life",     
+            ],
+            heading: "Here are a few highlights:"
+        }
+    }
+    const SCREEN_CONSTSANTS = {
+        description:
+        "Bernie is blah blah description",
+        highlights: {
+            bullets:[
+                "Bullet 1", 
+                "Bullet 2",
+                "Bullet 3",
+                "Such is life",   
+                "Such is life", 
+                "Such is life", 
+                "Such is life", 
+                "Such is life", 
+                "Such is life",   
+             ],
+          heading: "Here are a few highlights:",
+        },
+      };
+      const renderHighlight = () => {
+        return SCREEN_CONSTSANTS.highlights.bullets.map((value, i) => (
+          <div className="highlight" key={i}>
+            <div className="highlight-blob"></div>
+            <span>{value}</span>
+          </div>
+        ));
+      };
+    return (
+        <div className='about-me-container screen-container' id = {props.id || ""}>
+             <div className="about-me-parent">
         <ScreenHeading title={"About Me"} subHeading={"Why Choose Me?"} />
         <div className="about-me-card">
           <div className="about-me-profile"></div>
@@ -63,7 +74,7 @@ export default function AboutMe(props) {
                 {" "}
                 Hire Me{" "}
               </button>
-              <a href="ehizcv.pdf" download="Ehiedu Ehizcv.pdf">
+              <a href="Daniel Bernie Nel CV.pdf" download="Daniel Bernie Nel CV.pdf">
                 <button className="btn highlighted-btn">Get Resume</button>
               </a>
             </div>
---
+++ client/src/PortfolioContainer/Resume/Resume.js
@@ -0,0 +1,274 @@
+import React, { useState, useEffect } from "react";
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animations";
+import "./Resume.css";
+
+const Resume = (props) => {
+  /* different states */
+  const [selectedBulletIndex, setSelectedBulletIndex] = useState(0);
+  const [carousalOffsetStyle, setCarousalOffsetStyle] = useState({});
+
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
+
+    Animations.animations.fadeInScreen(props.id);
+  };
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  /* components on left of resume (categories) */
+  const ResumeHeading = (props) => {
+    return (
+      <div className="resume-heading">
+        <div className="resume-main-heading">
+          <div className="heading-bullet"></div>
+          <span>{props.heading ? props.heading : ""}</span>
+          {props.fromDate && props.toDate ? (
+            <div className="heading-date">
+              {props.fromDate + "-" + props.toDate}
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+        <div className="resume-sub-heading">
+          <span>{props.subHeading ? props.subHeading : ""}</span>
+        </div>
+        <div className="resume-heading-description">
+          <span>{props.description ? props.description : ""}</span>
+        </div>
+      </div>
+    )
+  };
+
+  /* STATIC RESUME DATA FOR THE LABELS*/
+  const resumeBullets = [
+    { label: "Education", logoSrc: "education.svg" },
+    { label: "Work History", logoSrc: "work-history.svg" },
+    { label: "Programming Skills", logoSrc: "programming-skills.svg" },
+    { label: "Projects", logoSrc: "projects.svg" },
+    { label: "Interests", logoSrc: "interests.svg" },
+  ];
+
+  //here we have
+  const programmingSkillsDetails = [
+    { skill: "C#", ratingPercentage: 90 },
+    { skill: "JavaScript", ratingPercentage: 72 },
+    { skill: "React JS", ratingPercentage: 83 },
+    { skill: "React Native", ratingPercentage: 80 },
+    { skill: "Python", ratingPercentage: 89 },
+    { skill: "Core Java", ratingPercentage: 85 },
+    { skill: "HTML", ratingPercentage: 82 },
+    { skill: "CSS", ratingPercentage: 80 },
+  ];
+
+  const projectsDetails = [
+    {
+      title: "A",
+      duration: { fromDate: "1", toDate: "2" },
+      description:
+        "P1",
+      subHeading: "P2",
+    },
+   
+  ];
+
+  const resumeDetails = [
+    <div className="resume-screen-container" key="education">
+      <ResumeHeading
+        heading={"North West University, Potchefstroom, South Africa"}
+        subHeading={"Bachelor of Science Information Technology"}
+        fromDate={"2019"}
+        toDate={"2021"}
+      />
+
+      <ResumeHeading
+        heading={"North West University, Potchefstroom, South Africa"}
+        subHeading={"Honors of Commerse Forensic Accounting"}
+        fromDate={"2019"}
+        toDate={"2019"}
+      />
+      <ResumeHeading
+        heading={"North West University, Potchefstroom, South Africa"}
+        subHeading={"Bachelor of Commerse Forensic Accounting"}
+        fromDate={"2016"}
+        toDate={"2018"}
+      />
+    </div>,
+    
+
+    /* WORK EXPERIENCE */
+    <div className="resume-screen-container" key="work-experience">
+      <div className="experience-container">
+        <ResumeHeading
+          heading={"Cyanre The Digital Forensic Lab"}
+          subHeading={"Digital Forensic Practitioner"}
+          fromDate={"Feb 2021 "}
+          toDate={" Present"}
+        />
+        
+        <div className="experience-description">
+          <span className="resume-description-text">
+            Currently working ...
+          </span>
+        </div>
+        <div className="experience-description">
+          <span className="resume-description-text">
+            - F1.
+          </span>
+          <br />
+          <br />
+
+          <ResumeHeading
+          heading={"TFS Africa"}
+          subHeading={"Forensic Investigator"}
+          fromDate={"Sept 2020 "}
+          toDate={" Apr 2021"}
+        />
+        <div className="experience-description">
+          <span className="resume-description-text">
+            Currently working ...
+          </span>
+          <br />
+          <br />
+        </div>
+
+        <ResumeHeading
+          heading={"ICA Consulting"}
+          subHeading={"Junior Forensic Assistant"}
+          fromDate={"Jan 2020 "}
+          toDate={" Aug 2020"}
+        />
+
+        <div className="experience-description">
+          <span className="resume-description-text">
+            Currently working ...
+          </span>
+          <br />
+        </div>
+
+        </div>
+      </div>
+    </div>,
+
+    /* PROGRAMMING SKILLS */
+    <div
+      className="resume-screen-container programming-skills-container"
+      key="programming-skills"
+    >
+      {programmingSkillsDetails.map((skill, index) => (
+        <div className="skill-parent" key={index}>
+          <div className="heading-bullet"></div>
+          <span>{skill.skill}</span>
+          <div className="skill-percentage">
+            <div
+              style={{ width: skill.ratingPercentage + "%" }}
+              className="active-percentage-bar"
+            ></div>
+          </div>
+        </div>
+      ))}
+    </div>,
+
+    /* PROJECTS */
+    <div className="resume-screen-container" key="projects">
+      {projectsDetails.map((projectsDetails, index) => (
+        <ResumeHeading
+          key={index}
+          heading={projectsDetails.title}
+          subHeading={projectsDetails.subHeading}
+          description={projectsDetails.description}
+          fromDate={projectsDetails.duration.fromDate}
+          toDate={projectsDetails.duration.toDate}
+        />
+      ))}
+    </div>,
+
+    /* Interests */
+    <div className="resume-screen-container" key="interests">
+      <ResumeHeading
+        heading="Music"
+        description="Apart from being a data and fraud analyst, i love playing guitar and drums. I also write and record songs in my free time."
+      />
+      <ResumeHeading
+        heading="Cooking"
+        description="I like cooking because"
+      />
+      <ResumeHeading
+        heading="Fishing"
+        description="I like "
+      />
+    </div>,
+  ];
+
+  const handleCarousal = (index) => {
+    let offsetHeight = 360;
+
+    let newCarousalOffset = {
+      style: { transform: "translateY(" + index * offsetHeight * -1 + "px)" },
+    };
+
+    setCarousalOffsetStyle(newCarousalOffset);
+    setSelectedBulletIndex(index);
+  };
+
+  const getBullets = () => {
+    return resumeBullets.map((bullet, index) => (
+      <div
+        onClick={() => handleCarousal(index)}
+        className={
+          index === selectedBulletIndex ? "bullet selected-bullet" : "bullet"
+        }
+        key={index}
+      >
+        <img
+          className="bullet-logo"
+          src={require(`../../assets/Resume/${bullet.logoSrc}`).default}
+          alt="B"
+        />
+        <span className="bullet-label">{bullet.label}</span>
+      </div>
+    ));
+  };
+
+  const getResumeScreens = () => {
+    return (
+      <div
+        style={carousalOffsetStyle.style}
+        className="resume-details-carousal"
+      >
+        {resumeDetails.map((ResumeDetail) => ResumeDetail)}
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    return () => {
+      /* unsub to subscription */
+      fadeInSubscription.unsubscribe();
+    };
+  }, [fadeInSubscription]);
+
+  return (
+    <div
+      className="resume-container screen-container" id={props.id || ""}
+    >
+      <div className="resume-content">
+        <ScreenHeading title={"Resume"} subHeading={"My formal Bio Details"} />
+        <div className="resume-card">
+          <div className="resume-bullets">
+            <div className="bullet-container">
+              <div className="bullet-icons"></div>
+              <div className="bullets">{getBullets()}</div>
+            </div>
+          </div>
+
+          <div className="resume-bullet-details">{getResumeScreens()}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
