0b471a0fb53e1508702a3176f43baed7c54910f9
dbernienel <71564993+dbernienel@users.noreply.github.com>
2021-12-05 13:29:08 +0200

    Changes to Contact me and footer

--- client/src/utilities/ScrollService.js
+++ client/src/utilities/ScrollService.js
@@ -20,7 +20,7 @@ export default class ScrollService{
     }
 
     scrollToHome = ()=>{
-        let homeScreen = document.getElementById("Hom")
+        let homeScreen = document.getElementById("Home")
         if (!homeScreen) return;
         homeScreen.scrollIntoView({behavior: "smooth"})
     }
